970220c6f7f280cbd4a7f608e423d2e81c43f781
Chris Smith <csmith@foundry.ai>
2020-12-14 18:46:44 -0500

    Add d13 solution

---
+++ d13/src/main.rs
@@ -0,0 +1,48 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+/// Find Manhattan distance from starting point after sequence of navigation instructions
+fn main() {
+    let mut arrival_time: u64 = 0;
+    let mut buses: Vec<u64> = Vec::new();
+    // Loop through instructions and change ship state
+    if let Ok(lines) = _read_lines("./input.txt") {
+        // if let Ok(lines) = _read_lines("./sample.txt") {
+        for line in lines {
+            if arrival_time == 0 {
+                arrival_time = line.unwrap().parse().unwrap();
+                continue;
+            }
+            let full_line = line.unwrap();
+            buses = full_line
+                .split(',')
+                .into_iter()
+                .filter_map(|bus| bus.parse().ok())
+                .collect();
+        }
+    }
+    let mut min_remainder = arrival_time;
+    let mut min_bus = 0;
+    for bus in buses {
+        let remainder = ((1 + (arrival_time / bus)) * bus) - arrival_time;
+        if remainder < min_remainder {
+            min_remainder = remainder;
+            min_bus = bus;
+        }
+    }
+    println!(
+        "best bus: {}, wait time: {}, product: {}",
+        min_bus,
+        min_remainder,
+        min_bus * min_remainder
+    );
+}
+
+fn _read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
